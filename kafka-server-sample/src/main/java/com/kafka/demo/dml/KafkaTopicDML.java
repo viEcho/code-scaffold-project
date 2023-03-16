e2048af1c5d14030f6e7792b8e50de0402576755
Jay <baoj0719@163.com>
2023-03-16 22:01:17 +0800

    23.0326-项目修改命名及jar包统一管理

---
+++ kafka-server-sample/src/main/java/com/kafka/demo/dml/KafkaTopicDML.java
@@ -0,0 +1,24 @@
+package com.kafka.demo.dml;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+
+import java.util.Properties;
+
+/**
+ * @description: kafka dml操作示例
+ * @author: echo
+ * @date: 2022/7/6
+ */
+public class KafkaTopicDML {
+
+    public static void main(String[] args) {
+        //创建kafkaAdminClient
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"CentOS101:9092,CentOS102:9092,CentOS103:9092");
+        KafkaAdminClient adminClient = (KafkaAdminClient) KafkaAdminClient.create(props);
+
+        adminClient.close();
+    }
+
+}
